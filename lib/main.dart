a9b1ce30b7df33d8d3d10f41883067a670271f92
sumin04 <gimsumin259@gmail.com>
2024-08-18 19:30:36 +0900

    홈 화면 디자인

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:tharmproject/widget/login.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white
+          // colorScheme: ColorScheme.fromSeed(
+          //     seedColor: const Color.fromARGB(255, 192, 145, 145)),
+          // useMaterial3: true,
+          ),
+      home: const Login(),
+    );
+  }
+}
